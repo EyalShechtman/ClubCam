72be14c96d43129c70b201dc9aee5b82e6185071
Eyal Shechtman <eyal.shechtman@gmail.com>
2025-03-04 00:32:42 -0800

    Initial Commit

---
+++ ClubCam/ClubCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClubCamApp.swift
+//  ClubCam
+//
+//  Created by Nadya Shechtman on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ClubCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClubCamTests/ClubCamTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClubCamTests.swift
+//  ClubCamTests
+//
+//  Created by Nadya Shechtman on 3/4/25.
+//
+
+import Testing
+@testable import ClubCam
+
+struct ClubCamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
